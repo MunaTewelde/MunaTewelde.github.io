ce6211e07f1adc477086d39c11cd23dcfe839288
MunaTewelde <mteklemariam@miu.edu>
2021-06-06 09:35:45 -0500

    numberEx

--- NumberEx/app.js
+++ NumberEx/app.js
@@ -1,23 +1,38 @@
-var http = require('http');
-var dt = require('./guess');
 
-http.createServer(function (req, res) {
-    res.writeHead(200,
-        { 'Content-Type': 'text/html' });
-    console.log("here......")
-    //  res.write("The date and time are currently: " + dt.guess());
-    res.write("<!DOCTYPE html>");
-    res.write("<html>");
-    res.write("<head><meta charset=\"utf-8\"/>");
-    res.write("<title>Calculator Web Site</title>");
-    res.write("</head>");
-    res.write("<body>");
-    res.write("<p>The sum is: ");
-    res.write(dt.guess());
-    res.write("</p>");
-    res.write("</body>");
-    res.write("</html>");
-    res.end();
-}).listen(8080);
-port = 8080;
-console.log("i am listning at port" + port)
+var express = require('express');
+var app = express();
+
+// define routes here..
+
+var server = app.listen(8080, function () {
+    console.log('Node server is running..');
+});
+
+
+
+
+
+// var http = require('http');
+// var dt = require('./guess');
+// const visited = "hiiii";
+// http.createServer(function (req, res) {
+//     res.writeHead(200,
+//         { 'Content-Type': 'text/html' });
+//     console.log("here......")
+//     //  res.write("The date and time are currently: " + dt.guess());
+//     res.render("parks", { visited: visited })
+//     // res.write("<!DOCTYPE html>");
+//     // res.write("<html>");
+//     // res.write("<head><meta charset=\"utf-8\"/>");
+//     // res.write("<title>Calculator Web Site</title>");
+//     // res.write("</head>");
+//     // res.write("<body>");
+//     // res.write("<p>The sum is: ");
+//     // res.write(dt.guess());
+//     // res.write("</p>");
+//     // res.write("</body>");
+//     // res.write("</html>");
+//     // res.end();
+// }).listen(8080);
+// port = 8080;
+// console.log("i am listning at port" + port)
--- NumberEx/guess.js
+++ NumberEx/guess.js
@@ -1,17 +1,18 @@
-exports.guess = function (req, res, vals) {
-    var sum = [[3, 1, 4, 1, 5], [3, 1, 4, 1, 5]]
-};
-// res.writeHead(200, { 'Content-Type': 'text/html' });
-// res.write("<!DOCTYPE html>");
-// res.write("<html>");
-// res.write("<head><meta charset=\"utf-8\"/>");
-// res.write("<title>Calculator Web Site</title>");
-// res.write("</head>");
-// res.write("<body>");
-// res.write("<p>The sum is: ");
-// res.write(String(sum));
-// res.write("</p>");
-// res.write("</body>");
-// res.write("</html>");
-return sum;
-}
+// exports.guess = function (req, res, vals) {
+//     var sum = [[3, 1, 4, 1, 5], [3, 1, 4, 1, 5]]
+//     var sample = "hello";
+//     // res.writeHead(200, { 'Content-Type': 'text/html' });
+//     // res.write("<!DOCTYPE html>");
+//     // res.write("<html>");
+//     // res.write("<head><meta charset=\"utf-8\"/>");
+//     // res.write("<title>Calculator Web Site</title>");
+//     // res.write("</head>");
+//     // res.write("<body>");
+//     // res.write("<p>The sum is: ");
+//     // res.write(String(sum));
+//     // res.write("</p>");
+//     // res.write("</body>");
+//     // res.write("</html>");
+//     res.render("parks", { visited: visited })
+//     return res.end();
+// };
